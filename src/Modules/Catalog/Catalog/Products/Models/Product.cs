f3d4d3b9e5bd5edbd3f3700bf2a91f51683d4f5d
Usman Tahir <Usman.tahir@synergy-it.com>
2025-06-24 11:03:56 +0500

    63. Develop Product Domain Entity Models in Catalog Module Domain Models

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using Shared.DDD;
+
+namespace Catalog.Products.Models
+{
+    public class Product :Entity<Guid>
+    {
+        public string Name { get; set; } = default!;
+        public List<string> Category { get; set; } = new();
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; } 
+    }
+}
