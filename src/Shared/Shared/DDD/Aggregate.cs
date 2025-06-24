f4f2942e704f1dae64af4cd6dc30220182faefc8
Usman Tahir <Usman.tahir@synergy-it.com>
2025-06-24 10:57:01 +0500

    61. Develop Aggregate classes in Tactical DDD Abstraction

---
+++ src/Shared/Shared/DDD/Aggregate.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Shared.DDD
+{
+    public class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            if (domainEvent == null)
+            {
+                throw new ArgumentNullException(nameof(domainEvent), "Domain event cannot be null.");
+            }
+            _domainEvents.Add(domainEvent);
+        }
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+            _domainEvents.Clear();
+            return dequeuedEvents;
+        }
+    }
+}
