a34876257819b9f4f3838b0444d15b101cf9df02
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-01 12:13:35 +0500

    109. Create Abstraction on MediatR for CQRS - Command and Query Separation - 2

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreatProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category,string Description,string ImageFile, decimal Price): IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreatProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
