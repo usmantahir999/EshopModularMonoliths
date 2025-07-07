cc87eeeb4680efef0e0c5131a8dee257e7dd7750
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-08 00:46:32 +0500

    136. Register Carter libraries into Asp.Net Dependency Injection Service and Request

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,7 +1,14 @@
 
 
-var builder = WebApplication.CreateBuilder(args);
+using Carter;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter(configurator: config =>
+{
+    var catalogModules = typeof(CatalogModule).Assembly.GetTypes()
+        .Where(t =>  t.IsAssignableTo(typeof(ICarterModule))).ToArray();
+    config.WithModules(catalogModules);
+});
 //Add services to the container
 builder.Services.AddBasketModule(builder.Configuration)
                 .AddCatalogModule(builder.Configuration)
@@ -9,6 +16,7 @@ builder.Services.AddBasketModule(builder.Configuration)
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
+app.MapCarter();
 app.UseCatalogModule()
                 .UseBasketModule()
                 .UseOrderingModule();
