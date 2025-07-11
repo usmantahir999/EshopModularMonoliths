d7883bb52ad65093b748c6083d792c0455cb1be8
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-11 15:25:35 +0500

    157. Structured Logging Using Serilog in .NET 8.0 Minimal APIs

--- src/Bootstrapper/Api/GlobalUsings.cs
+++ src/Bootstrapper/Api/GlobalUsings.cs
@@ -3,3 +3,5 @@ global using Catalog;
 global using Ordering;
 global using Carter;
 global using Shared.Extensions;
+global using Serilog;
+global using Shared.Exceptions.Handler;
--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,7 +1,7 @@
 
-using Shared.Exceptions.Handler;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
 builder.Services.AddCarterWithAssemblies(typeof(CatalogModule).Assembly);
 //Add services to the container
 builder.Services.AddBasketModule(builder.Configuration)
@@ -12,10 +12,13 @@ var app = builder.Build();
 
 //Configure the HTTP request pipeline.
 app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseSerilogRequestLogging();
+
 app.UseCatalogModule()
                 .UseBasketModule()
                 .UseOrderingModule();
 
-app.UseExceptionHandler(options => { });
+
 app.Run();
  
