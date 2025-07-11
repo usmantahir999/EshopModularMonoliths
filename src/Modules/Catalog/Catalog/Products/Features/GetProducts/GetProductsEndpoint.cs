2a70fcb73fec747ac647980dfeeb63f4ba722a65
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-11 16:32:36 +0500

    160. Develop GetProducts Query apply Pagination using CQRS and MediatR

--- src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
@@ -1,23 +1,28 @@
 ﻿
+using Shared.Pagination;
+
 namespace Catalog.Products.Features.GetProducts
 {
     //public record GetProductsRequest();
-    public record GetProductsResponse(IEnumerable<ProductDto> Products);
+    public record GetProductsResponse(PaginatedResult<ProductDto> Products);
 
     public class GetProductsEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/products", async (ISender sender) =>
+            app.MapGet("/products", async ([AsParameters] PaginationRequest request, ISender sender) =>
             {
-                var result = await sender.Send(new GetProductsQuery());
+                var result = await sender.Send(new GetProductsQuery(request));
+
                 var response = result.Adapt<GetProductsResponse>();
+
                 return Results.Ok(response);
-            }).WithName("GetProducts")
-        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
-        .ProducesProblem(StatusCodes.Status400BadRequest)
-        .WithSummary("Get Products")
-        .WithDescription("Get Products");
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
         }
     }
 }
--- src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
@@ -1,18 +1,41 @@
-﻿namespace Catalog.Products.Features.GetProducts
+﻿using Shared.Pagination;
+
+namespace Catalog.Products.Features.GetProducts
 {
-    public record GetProductsQuery : IQuery<GetProductResult>;
+    public record GetProductsQuery(PaginationRequest PaginationRequest)
+    : IQuery<GetProductsResult>;
+    public record GetProductsResult(PaginatedResult<ProductDto> Products);
 
-    public record GetProductResult(IEnumerable<ProductDto> Products);
-
-    internal class GetProductsHandler(CatalogDbContext dbContext) : IQueryHandler<GetProductsQuery, GetProductResult>
+    internal class GetProductsHandler(CatalogDbContext dbContext)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
-        public async Task<GetProductResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            var products = await dbContext.Products.AsNoTracking().OrderBy(p => p.Name).ToListAsync(cancellationToken);
-            var productDtos = products.Adapt<List<ProductDto>>();
-            return new GetProductResult(productDtos);
-        }
+            // get products using dbContext
+            // return result
 
-        
+            var pageIndex = query.PaginationRequest.PageIndex;
+            var pageSize = query.PaginationRequest.PageSize;
+
+            var totalCount = await dbContext.Products.LongCountAsync(cancellationToken);
+
+            var products = await dbContext.Products
+                            .AsNoTracking()
+                            .OrderBy(p => p.Name)
+                            .Skip(pageSize * pageIndex)
+                            .Take(pageSize)
+                            .ToListAsync(cancellationToken);
+
+            //mapping product entity to ProductDto using Mapster
+            var productDtos = products.Adapt<List<ProductDto>>();
+
+            return new GetProductsResult(
+                new PaginatedResult<ProductDto>(
+                    pageIndex,
+                    pageSize,
+                    totalCount,
+                    productDtos)
+                );
+        }
     }
 }
