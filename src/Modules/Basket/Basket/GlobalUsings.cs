dd332846cbc5bd7d121e44cc05044b809547a269
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-15 17:58:03 +0500

    168. Develop EF Core DBContext Object for Storing Entities in Basket Infrastructure

--- src/Modules/Basket/Basket/GlobalUsings.cs
+++ src/Modules/Basket/Basket/GlobalUsings.cs
@@ -2,3 +2,6 @@
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Shared.DDD;
+global using Basket.Basket.Models;
+global using Microsoft.EntityFrameworkCore;
+global using System.Reflection;
