1a107aec787196e4782ce1f49c42e698753901fb
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-17 13:15:08 +0500

    187. Develop Basket RemoveItemFromBasketHandler with CQRS and MediatR

--- src/Modules/Basket/Basket/Basket/Features/RemoveItemFromBasket/RemoveItemFromBasketHandler.cs
+++ src/Modules/Basket/Basket/Basket/Features/RemoveItemFromBasket/RemoveItemFromBasketHandler.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Basket.Basket.Features.RemoveItemFromBasket
 {
-    internal class RemoveItemFromBasketHandler
+    public record RemoveItemFromBasketCommand(string UserName, Guid ProductId)
+    : ICommand<RemoveItemFromBasketResult>;
+    public record RemoveItemFromBasketResult(Guid Id);
+    public class RemoveItemFromBasketCommandValidator : AbstractValidator<RemoveItemFromBasketCommand>
     {
+        public RemoveItemFromBasketCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+            RuleFor(x => x.ProductId).NotEmpty().WithMessage("ProductId is required");
+        }
+    }
+    internal class RemoveItemFromBasketHandler(BasketDbContext dbContext) : ICommandHandler<RemoveItemFromBasketCommand, RemoveItemFromBasketResult>
+    {
+        public async Task<RemoveItemFromBasketResult> Handle(RemoveItemFromBasketCommand command, CancellationToken cancellationToken)
+        {
+            var shoppingCart = await dbContext.ShoppingCarts
+                .Include(x => x.Items)
+                .SingleOrDefaultAsync(x => x.UserName == command.UserName, cancellationToken);
+            if (shoppingCart is null)
+            {
+                throw new BasketNotFoundException(command.UserName);
+            }
+            shoppingCart.RemoveItem(command.ProductId);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return new RemoveItemFromBasketResult(shoppingCart.Id);
+        }
     }
 }
