a0c7711f42edba4a1c30080bb55bb7f964987cb6
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-22 12:55:22 +0500

    203. Develop BasketRepository Class using EF Core DbContext object

---
+++ src/Modules/Basket/Basket/Data/Repository/BasketRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.Data.Repository
+{
+    internal class BasketRepository : IBasketRepository
+    {
+        public Task<ShoppingCart> GetBasket(string userName, bool asNoTracking = true, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ShoppingCart> CreateBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
