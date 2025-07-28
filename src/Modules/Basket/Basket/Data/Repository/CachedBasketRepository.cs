bc20b9c2bc288cc66408236b1f13afad4f30eb4e
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-28 11:35:50 +0500

    212. Develop CachedBasketRepository with Proxy Pattern, Decorator Pattern - Part 2

---
+++ src/Modules/Basket/Basket/Data/Repository/CachedBasketRepository.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.Data.Repository
+{
+    public class CachedBasketRepository(IBasketRepository basketRepository, IDistributedCache cache) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string userName, bool asNoTracking = true, CancellationToken cancellationToken = default)
+        {
+            if (!asNoTracking)
+            {
+                return await basketRepository.GetBasket(userName, asNoTracking, cancellationToken);
+            }
+
+            var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+            if(!string.IsNullOrEmpty(cachedBasket))
+            {
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+            }
+            var basket = await basketRepository.GetBasket(userName, asNoTracking, cancellationToken);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+
+        }
+        public async Task<ShoppingCart> CreateBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            await basketRepository.CreateBasket(basket, cancellationToken);
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            await basketRepository.DeleteBasket(userName, cancellationToken);
+            await cache.RemoveAsync(userName, cancellationToken);
+            return true;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await basketRepository.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
