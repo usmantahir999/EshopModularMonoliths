e6929eb083cda1cf40a986e288b9e84083b8a50e
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-31 15:51:54 +0500

    219. Develop Custom JsonConverter for ShoppingCart and Items When Reading to Redis

--- src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Text.Json.Serialization;
+
 namespace Basket.Basket.Models
 {
     public class ShoppingCartItem :Entity<Guid>
@@ -13,6 +15,18 @@ namespace Basket.Basket.Models
 
         internal ShoppingCartItem(Guid shoppingCartId, Guid productId, int quantity, string color, decimal price, string productName)
         {
+            ShoppingCartId = shoppingCartId;
+            ProductId = productId;
+            Quantity = quantity;
+            Color = color;
+            Price = price;
+            ProductName = productName;
+        }
+
+        [JsonConstructor]
+        public ShoppingCartItem(Guid id, Guid shoppingCartId, Guid productId, int quantity, string color, decimal price, string productName)
+        {
+            Id = id;
             ShoppingCartId = shoppingCartId;
             ProductId = productId;
             Quantity = quantity;
