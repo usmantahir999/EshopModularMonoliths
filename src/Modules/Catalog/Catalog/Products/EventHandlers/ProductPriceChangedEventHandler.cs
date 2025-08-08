d8dadb9fa8576881e4546e9fd9508dec966b3fb0
Usman Tahir <Usman.tahir@synergy-it.com>
2025-08-08 18:07:56 +0500

    251. Step3: Catalog Publish UpdatePrice Integration Event

--- src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
+++ src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -1,11 +1,26 @@
-﻿namespace Catalog.Products.EventHandlers
+﻿using MassTransit;
+using Shared.Messaging.Events;
+
+namespace Catalog.Products.EventHandlers
 {
-    public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger) : INotificationHandler<ProductPriceChangedEvent>
+    public class ProductPriceChangedEventHandler(IBus bus,ILogger<ProductPriceChangedEventHandler> logger) : INotificationHandler<ProductPriceChangedEvent>
     {
         public async Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
         {
             logger.LogInformation("Domain event handled: {DomainEvent}", notification.GetType().Name);
-            await Task.CompletedTask;
+            
+            // Publish product price changed integration event
+            var integrationEvent = new ProductPriceChangedIntegrationEvent
+            {
+                ProductId = notification.Product.Id,
+                Name = notification.Product.Name,
+                Category = notification.Product.Category,
+                Description = notification.Product.Description,
+                ImageFile = notification.Product.ImageFile,
+                Price = notification.Product.Price
+            };
+
+            await bus.Publish(integrationEvent, cancellationToken);
         }
     }
 }
