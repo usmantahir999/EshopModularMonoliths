f9e63b656e0a51c333a6fc138697eb05f2fa5faa
Usman Tahir <Usman.tahir@synergy-it.com>
2025-08-08 17:58:39 +0500

    248. Add Project Reference to Shared.Messaging in Catalog and Basket Modules

--- src/Shared/Shared.Messaging/Shared.Messaging/Extensions/MassTransitExtensions.cs
+++ src/Shared/Shared.Messaging/Shared.Messaging/Extensions/MassTransitExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Shared.Messaging.Extensions
@@ -7,6 +8,19 @@ namespace Shared.Messaging.Extensions
     {
         public static IServiceCollection AddMassTransitExtensions(this IServiceCollection services, params Assembly[] assemblies)
         {
+            services.AddMassTransit(config =>
+            {
+                config.SetKebabCaseEndpointNameFormatter();
+                config.SetInMemorySagaRepositoryProvider();
+                config.AddConsumers(assemblies);
+                config.AddSagas(assemblies);
+                config.AddActivities(assemblies);
+
+                config.UsingInMemory((context, cfg) =>
+                {
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
             return services;
         }
     }
