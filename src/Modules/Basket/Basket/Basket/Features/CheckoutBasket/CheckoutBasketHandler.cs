977002061f9295b9444f7a4a738d33ee3c833f6c
Usman Tahir <Usman.tahir@synergy-it.com>
2025-09-02 14:29:08 +0500

    291. Develop BasketCheckout Feature Folder to Expose POST Endpoint and Publish Event

---
+++ src/Modules/Basket/Basket/Basket/Features/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.Basket.Features.CheckoutBasket
+{
+    public record CheckoutBasketCommand(BasketCheckoutDto BasketCheckout)
+    : ICommand<CheckoutBasketResult>;
+
+    public record CheckoutBasketResult(bool IsSuccess);
+
+    public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCommand>
+    {
+        public CheckoutBasketCommandValidator()
+        {
+            RuleFor(x => x.BasketCheckout).NotNull().WithMessage("BasketCheckoutDto can't be null");
+            RuleFor(x => x.BasketCheckout.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+}
